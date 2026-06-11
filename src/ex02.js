ea1dae2e3db77de8b4280150834aa2f3bf946951
Miru-N <ito.online.shop@gmail.com>
2026-06-11 09:56:52 -0700

    Complete exercise 2

--- src/ex02.js
+++ src/ex02.js
@@ -4,7 +4,19 @@
  * @returns {number}
  */
 function sumArray(arr) {
-  return arr.reduce((total, number) => total + number);
+  if (arr === undefined) {
+    throw new Error('Array cannot be undefined');
+  }
+
+  if (!Array.isArray(arr)) {
+    throw new Error('Argument must be an array');
+  }
+
+  if (arr.some(item => typeof item !== 'number')) {
+    throw new Error('Array can only contain numbers');
+  }
+
+  return arr.reduce((total, number) => total + number, 0);
 }
 
 module.exports = sumArray;
